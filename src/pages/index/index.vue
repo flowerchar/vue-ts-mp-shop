1620cb8e3053a69025ac99777f162c48acdac208
flowerchar <flowerchar2001@gmail.com>
2023-09-04 20:41:54 +0800

    feat: 小程序端pinia持久化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,10 +4,14 @@
 
 <template>
   <view class="index">index</view>
-  <uni-card>
-    <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
+  <uni-card
+    title="基础卡片"
+    sub-title="副标题"
+    extra="额外信息"
+    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+  >
+    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
   </uni-card>
-
 </template>
 
 <style lang="scss">
--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -27,6 +27,15 @@ export const useMemberStore = defineStore(
   },
   // TODO: 持久化
   {
-    persist: true,
+    persist: {
+      storage: {
+        getItem(key) {
+          return uni.getStorageSync(key)
+        },
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
+        },
+      },
+    },
   },
 )
