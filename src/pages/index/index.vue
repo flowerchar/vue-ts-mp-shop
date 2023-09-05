e7922ef30d4fcb19560690d7e10a8697db59e33f
flowerchar <flowerchar2001@gmail.com>
2023-09-05 13:56:30 +0800

    feat: 首页-自定义导航栏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
 //
+
+import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <CustomNavbar />
   <view class="index">index</view>
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-  >
-    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
 </template>
 
 <style lang="scss">
