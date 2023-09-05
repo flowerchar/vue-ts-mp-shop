2aa380e1499af2a136ecc530f2f746ab5e3380f2
flowerchar <flowerchar2001@gmail.com>
2023-09-05 14:18:58 +0800

    轮播图-指示点

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -2,11 +2,14 @@
 import { ref } from 'vue'
 
 const activeIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  activeIndex.value = ev.detail!.current
+}
 </script>
 
 <template>
   <view class="carousel">
-    <swiper :circular="true" :autoplay="false" :interval="3000">
+    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
       <swiper-item>
         <navigator url="/pages/index/index" hover-class="none" class="navigator">
           <image
