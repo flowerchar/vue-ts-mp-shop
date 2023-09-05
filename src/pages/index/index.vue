ae6e9a8f9bdbc0570bad6b73087f9b3df7eaf069
flowerchar <flowerchar2001@gmail.com>
2023-09-05 15:06:40 +0800

    feat: 轮播图-数据类型定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
 //
-
+import type { BannerItem } from '@/types/home'
 import CustomNavbar from './components/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import { getHomeBannerAPI } from '@/services/home'
 
-const bannerList = ref([])
+const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,7 +1,8 @@
+import type { BannerItem } from '@/types/home'
 import { http } from '@/utils/http'
 
 export const getHomeBannerAPI = (distributionSite = 1) => {
-  return http({
+  return http<BannerItem[]>({
     method: 'GET',
     url: '/home/banner',
     data: {
