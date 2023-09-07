6c8085891e22365c88fa9db63b8f11be6541afc7
flowerchar <flowerchar2001@gmail.com>
2023-09-07 11:51:35 +0800

    feat: 热门推荐-类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,9 +1,10 @@
 import type { PageParams } from '@/types/global'
+import type { HotResult } from '@/types/hot'
 import { http } from '@/utils/http'
 
 type HotParams = PageParams & { subType?: string }
 export const getHotRecommendAPI = (url: string, data?: HotParams) => {
-  return http({
+  return http<HotResult>({
     method: 'GET',
     url,
     data,
