dc26b60a0767a85da49d20d122f3bb6cf25bc647
flowerchar <flowerchar2001@gmail.com>
2023-09-07 23:02:46 +0800

    feat: 商品分类-页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,7 +1,8 @@
+import type { GoodsResult } from '@/types/goods'
 import { http } from '@/utils/http'
 
 export const getGoodsByIdAPI = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: {
