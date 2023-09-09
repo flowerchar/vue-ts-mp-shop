ed14dbfde8fc68bea68e9936e61807b2d432a5aa
flowerchar <flowerchar2001@gmail.com>
2023-09-09 17:45:50 +0800

    feat: 商品详情-轮播图交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -13,10 +13,22 @@ const goods = ref<GoodsResult>()
 const getGoodsByIdData = async () => {
   const res = await getGoodsByIdAPI(query.id)
   goods.value = res.result
+  //TODO 为什么会有undefined
 }
 onLoad(() => {
   getGoodsByIdData()
 })
+
+const currentIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currentIndex.value = ev.detail.current //!
+}
+const onTapImage = (url: string) => {
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures,
+  })
+}
 </script>
 
 <template>
@@ -25,15 +37,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper circular @change="onChange">
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" /> </swiper-item
+            <image mode="aspectFill" :src="item" @tap="onTapImage(item)" /> </swiper-item
           >>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
