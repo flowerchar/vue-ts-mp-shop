0904ed84b87235aa280590aa9f21926c29c38551
flowerchar <flowerchar2001@gmail.com>
2023-09-10 20:31:56 +0800

    feat: 会员中心-退出登录

--- src/pagesMember/settings/settings.vue
+++ src/pagesMember/settings/settings.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
+import { useMemberStore } from '@/stores'
+
 //
+const memberStore = useMemberStore()
+const onLogout = () => {
+  uni.showModal({
+    content: '是否退出登录',
+    success: (res) => {
+      if (res.confirm) {
+        memberStore.clearProfile()
+        uni.navigateBack()
+      }
+    },
+  })
+}
 </script>
 
 <template>
   <view class="viewport">
     <!-- 列表1 -->
-    <view class="list" v-if="true">
+    <view class="list" v-if="memberStore.profile">
       <navigator url="/pagesMember/address/address" hover-class="none" class="item arrow">
         我的收货地址
       </navigator>
@@ -21,8 +35,8 @@
       <navigator hover-class="none" class="item arrow" url=" ">关于小兔鲜儿</navigator>
     </view>
     <!-- 操作按钮 -->
-    <view class="action">
-      <view class="button">退出登录</view>
+    <view class="action" v-if="memberStore.profile">
+      <view class="button" @tap="onLogout">退出登录</view>
     </view>
   </view>
 </template>
